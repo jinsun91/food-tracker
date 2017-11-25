22627670f22fcdc8ff79f5028c14a727029b6a6e
jinsun91 <jinsunpark917@gmail.com>
2017-11-25 17:35:23 +0000

    added camera functionality

--- app/src/main/java/jspark/foodtracker/MainActivity.java
+++ app/src/main/java/jspark/foodtracker/MainActivity.java
@@ -1,7 +1,10 @@
 package jspark.foodtracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button cameraButton = (Button) findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), CameraActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
