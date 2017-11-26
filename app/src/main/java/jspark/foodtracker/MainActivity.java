da4f7d012697e5cb31e91518d2bd981f654da80b
Jen <jen@Jenny-Mac.local>
2017-11-26 02:07:35 +0000

    [jenny] Testing of api completed

--- app/src/main/java/jspark/foodtracker/MainActivity.java
+++ app/src/main/java/jspark/foodtracker/MainActivity.java
@@ -1,15 +1,24 @@
 package jspark.foodtracker;
 
 import android.content.Intent;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        Recognizer recognizer = new Recognizer();
+        recognizer.recognizer();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -22,6 +31,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-
     }
+
 }
