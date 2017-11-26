9db1ebf581ac16a203b9e08d17339789d81b4e0b
Cheryl  Chen <cheryl.chen16@imperial.ac.uk>
2017-11-26 00:44:48 +0000

    AHHHHHHHHHHHHHHH NOOOO

--- app/src/main/java/jspark/foodtracker/MainActivity.java
+++ app/src/main/java/jspark/foodtracker/MainActivity.java
@@ -1,18 +1,37 @@
 package jspark.foodtracker;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toolbar;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView simpleList;
+    private List<String> foodList = new ArrayList<>();
 
+    @TargetApi(Build.VERSION_CODES.O)
+    public MainActivity(){
+        FoodItem foodItem = new FoodItem("Chicken Nugget", LocalDateTime.now());
+        addFoodItem(foodItem);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.activity_listview, R.id.textView, foodList);
+        simpleList.setAdapter(arrayAdapter);
         Button cameraButton = (Button) findViewById(R.id.cameraButton);
         cameraButton.setOnClickListener(new View.OnClickListener() {
 
@@ -22,6 +41,11 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-
     }
+
+    public void addFoodItem(FoodItem foodItem){
+        foodList.add(foodItem.toString());
+    }
+
+
 }
